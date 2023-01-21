7ec3f834ac83ab57defbd9143fa7f807998c94d4
Kerry Su <me@sshockwave.net>
2023-01-21 16:11:05 +0800

    Fix cython patch

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,15 +1,16 @@
 set -ex
 
-# Patch Cython such that it can be executed using Python
-mv "
-$BUILD_PREFIX/bin/python
+if [[ "$target_platform" == "osx-arm64" ]]
+then
+  # Patch Cython such that it can be executed using Python
+  old_cy="$BUILD_PREFIX/bin/cython"
+  bak_cy="$BUILD_PREFIX/bin/cython_bak"
+  echo "\"$BUILD_PREFIX/bin/python-$PY_VER\" \"$bak_cy\" \"\$@\"" > "$old_cy"
+  chmod +x "$old_cy"
+fi
 
 ./autogen.sh
 ./configure --prefix="$PREFIX" --enable-shared --disable-static
-
-which cython #DEBUG
-ls -l $(which cython) #DEBUG
-ls -l "$BUILD_PREFIX/bin" #DEBUG
 
 make -j${CPU_COUNT}
 make install
