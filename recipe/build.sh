3d3eb9f4571b842b0f6376d038be25d824936ddd
Kerry Su <me@sshockwave.net>
2023-01-21 15:31:09 +0800

    Check permission errors for cython

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,8 @@ set -ex
 ./configure --prefix="$PREFIX" --enable-shared --disable-static
 
 which cython #DEBUG
-cat $(which cython) #DEBUG
+ls -l $(which cython) #DEBUG
+ls -l "$BUILD_PREFIX/bin" #DEBUG
 
 make -j${CPU_COUNT}
 make install
